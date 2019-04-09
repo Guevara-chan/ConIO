911859e32ba58a8bcf20aae69e91b7d0bc8b71e9
Victoria Guevara <Guevara-chan@mail.ru>
2019-04-09 23:59:19 +0300

    Add files via upload

--- examples/hello_world.nim
+++ examples/hello_world.nim
@@ -8,6 +8,7 @@ con.title = "•Con/IO test•"
 for t in 0..8:
     con.setCursorPosition t, t
     con.foregroundcolor = cast[con.colors](t + 7)
-    con.log "Hello, world !"
+    con.log "Hello, world ! Привет, мир."
+con.log con.readLine
 con.beep()
 discard con.read_key 
